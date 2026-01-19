425510abf57f95858707d7030d96b3eb2a0f2ab9
banksray206 <banksray206@gmail.com>
2026-01-19 18:45:47 +0300

    2

--- e-commerce/src/main.jsx
+++ e-commerce/src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {BrowerRouter} from 'react-router';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowerRouter>
+      <App/>
+    </BrowerRouter>
+   
   </StrictMode>,
 )
