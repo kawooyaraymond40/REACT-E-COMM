ea05b7489a3be2f1e589a71852d4e35a60b9ee73
banksray206 <banksray206@gmail.com>
2026-01-26 22:44:09 +0300

    checkoutpage to react

--- e-commerce/src/pages/CheckoutPage.jsx
+++ e-commerce/src/pages/CheckoutPage.jsx
@@ -1,98 +1,102 @@
+import './checkout-header.css';
+import './checkoutPage.css';
+
 export function CheckoutPage() {
    return  <>
-        <div clasName ="checkout-header">
-            <div clasName ="header-content">
-                <div clasName ="checkout-header-left-section">
+   <title>Checkout</title>
+        <div className="checkout-header">
+            <div className="header-content">
+                <div className="checkout-header-left-section">
                     <a href="index.html">
-                        <img clasName ="logo" src="images/logo.png" />
-                        <img clasName ="mobile-logo" src="images/mobile-logo.png" />
+                        <img className="logo" src="images/logo.png" />
+                        <img className="mobile-logo" src="images/mobile-logo.png" />
                     </a>
                 </div>
 
-                <div clasName ="checkout-header-middle-section">
-                    Checkout (<a clasName ="return-to-home-link"
+                <div className="checkout-header-middle-section">
+                    Checkout (<a className="return-to-home-link"
                         href="index.html">3 items</a>)
                 </div>
 
-                <div clasName ="checkout-header-right-section">
+                <div className="checkout-header-right-section">
                     <img src="images/icons/checkout-lock-icon.png" />
                 </div>
             </div>
         </div>
 
-        <div clasName ="checkout-page">
-            <div clasName ="page-title">Review your order</div>
+        <div className="checkout-page">
+            <div className="page-title">Review your order</div>
 
-            <div clasName ="checkout-grid">
-                <div clasName ="order-summary">
-                    <div clasName ="cart-item-container">
-                        <div clasName ="delivery-date">
+            <div className="checkout-grid">
+                <div className="order-summary">
+                    <div className="cart-item-container">
+                        <div className="delivery-date">
                             Delivery date: Tuesday, June 21
                         </div>
 
-                        <div clasName ="cart-item-details-grid">
-                            <img clasName ="product-image"
+                        <div className="cart-item-details-grid">
+                            <img className="product-image"
                                 src="images/products/athletic-cotton-socks-6-pairs.jpg" />
 
-                            <div clasName ="cart-item-details">
-                                <div clasName ="product-name">
+                            <div className="cart-item-details">
+                                <div className="product-name">
                                     Black and Gray Athletic Cotton Socks - 6 Pairs
                                 </div>
-                                <div clasName ="product-price">
+                                <div className="product-price">
                                     $10.90
                                 </div>
-                                <div clasName ="product-quantity">
+                                <div className="product-quantity">
                                     <span>
-                                        Quantity: <span clasName ="quantity-label">2</span>
+                                        Quantity: <span className="quantity-label">2</span>
                                     </span>
-                                    <span clasName ="update-quantity-link link-primary">
+                                    <span className="update-quantity-link link-primary">
                                         Update
                                     </span>
-                                    <span clasName ="delete-quantity-link link-primary">
+                                    <span className="delete-quantity-link link-primary">
                                         Delete
                                     </span>
                                 </div>
                             </div>
 
-                            <div clasName ="delivery-options">
-                                <div clasName ="delivery-options-title">
+                            <div className="delivery-options">
+                                <div className="delivery-options-title">
                                     Choose a delivery option:
                                 </div>
-                                <div clasName ="delivery-option">
+                                <div className="delivery-option">
                                     <input type="radio" checked
-                                        clasName ="delivery-option-input"
+                                        className="delivery-option-input"
                                         name="delivery-option-1" />
                                     <div>
-                                        <div clasName ="delivery-option-date">
+                                        <div className="delivery-option-date">
                                             Tuesday, June 21
                                         </div>
-                                        <div clasName ="delivery-option-price">
+                                        <div className="delivery-option-price">
                                             FREE Shipping
                                         </div>
                                     </div>
                                 </div>
-                                <div clasName ="delivery-option">
+                                <div className="delivery-option">
                                     <input type="radio"
-                                        clasName ="delivery-option-input"
+                                        className="delivery-option-input"
                                         name="delivery-option-1" />
                                     <div>
-                                        <div clasName ="delivery-option-date">
+                                        <div className="delivery-option-date">
                                             Wednesday, June 15
                                         </div>
-                                        <div clasName ="delivery-option-price">
+                                        <div className="delivery-option-price">
                                             $4.99 - Shipping
                                         </div>
                                     </div>
                                 </div>
-                                <div clasName ="delivery-option">
+                                <div className="delivery-option">
                                     <input type="radio"
-                                        clasName ="delivery-option-input"
+                                        className="delivery-option-input"
                                         name="delivery-option-1" />
                                     <div>
-                                        <div clasName ="delivery-option-date">
+                                        <div className="delivery-option-date">
                                             Monday, June 13
                                         </div>
-                                        <div clasName ="delivery-option-price">
+                                        <div className="delivery-option-price">
                                             $9.99 - Shipping
                                         </div>
                                     </div>
@@ -101,72 +105,72 @@ export function CheckoutPage() {
                         </div>
                     </div>
 
-                    <div clasName ="cart-item-container">
-                        <div clasName ="delivery-date">
+                    <div className="cart-item-container">
+                        <div className="delivery-date">
                             Delivery date: Wednesday, June 15
                         </div>
 
-                        <div clasName ="cart-item-details-grid">
-                            <img clasName ="product-image"
+                        <div className="cart-item-details-grid">
+                            <img className="product-image"
                                 src="images/products/intermediate-composite-basketball.jpg" />
 
-                            <div clasName ="cart-item-details">
-                                <div clasName ="product-name">
+                            <div className="cart-item-details">
+                                <div className="product-name">
                                     Intermediate Size Basketball
                                 </div>
-                                <div clasName ="product-price">
+                                <div className="product-price">
                                     $20.95
                                 </div>
-                                <div clasName ="product-quantity">
+                                <div className="product-quantity">
                                     <span>
-                                        Quantity: <span clasName ="quantity-label">1</span>
+                                        Quantity: <span className="quantity-label">1</span>
                                     </span>
-                                    <span clasName ="update-quantity-link link-primary">
+                                    <span className="update-quantity-link link-primary">
                                         Update
                                     </span>
-                                    <span clasName ="delete-quantity-link link-primary">
+                                    <span className="delete-quantity-link link-primary">
                                         Delete
                                     </span>
                                 </div>
                             </div>
 
-                            <div clasName ="delivery-options">
-                                <div clasName ="delivery-options-title">
+                            <div className="delivery-options">
+                                <div className="delivery-options-title">
                                     Choose a delivery option:
                                 </div>
 
-                                <div clasName ="delivery-option">
-                                    <input type="radio" clasName ="delivery-option-input"
+                                <div className="delivery-option">
+                                    <input type="radio" className="delivery-option-input"
                                         name="delivery-option-2" />
                                     <div>
-                                        <div clasName ="delivery-option-date">
+                                        <div className="delivery-option-date">
                                             Tuesday, June 21
                                         </div>
-                                        <div clasName ="delivery-option-price">
+                                        <div className="delivery-option-price">
                                             FREE Shipping
                                         </div>
                                     </div>
                                 </div>
-                                <div clasName ="delivery-option">
-                                    <input type="radio" checked clasName ="delivery-option-input"
+                                <div className="delivery-option">
+                                    <input type="radio" checked className="delivery-option-input"
                                         name="delivery-option-2" />
                                     <div>
-                                        <div clasName ="delivery-option-date">
+                                        <div className="delivery-option-date">
                                             Wednesday, June 15
                                         </div>
-                                        <div clasName ="delivery-option-price">
+                                        <div className="delivery-option-price">
                                             $4.99 - Shipping
                                         </div>
                                     </div>
                                 </div>
-                                <div clasName ="delivery-option">
-                                    <input type="radio" clasName ="delivery-option-input"
+                                <div className="delivery-option">
+                                    <input type="radio" className="delivery-option-input"
                                         name="delivery-option-2" />
                                     <div>
-                                        <div clasName ="delivery-option-date">
+                                        <div className="delivery-option-date">
                                             Monday, June 13
                                         </div>
-                                        <div clasName ="delivery-option-price">
+                                        <div className="delivery-option-price">
                                             $9.99 - Shipping
                                         </div>
                                     </div>
@@ -176,37 +180,37 @@ export function CheckoutPage() {
                     </div>
                 </div>
 
-                <div clasName ="payment-summary">
-                    <div clasName ="payment-summary-title">
+                <div className="payment-summary">
+                    <div className="payment-summary-title">
                         Payment Summary
                     </div>
 
-                    <div clasName ="payment-summary-row">
+                    <div className="payment-summary-row">
                         <div>Items (3):</div>
-                        <div clasName ="payment-summary-money">$42.75</div>
+                        <div className="payment-summary-money">$42.75</div>
                     </div>
 
-                    <div clasName ="payment-summary-row">
+                    <div className="payment-summary-row">
                         <div>Shipping &amp; handling:</div>
-                        <div clasName ="payment-summary-money">$4.99</div>
+                        <div className="payment-summary-money">$4.99</div>
                     </div>
 
-                    <div clasName ="payment-summary-row subtotal-row">
+                    <div className="payment-summary-row subtotal-row">
                         <div>Total before tax:</div>
-                        <div clasName ="payment-summary-money">$47.74</div>
+                        <div className="payment-summary-money">$47.74</div>
                     </div>
 
-                    <div clasName ="payment-summary-row">
+                    <div className="payment-summary-row">
                         <div>Estimated tax (10%):</div>
-                        <div clasName ="payment-summary-money">$4.77</div>
+                        <div className="payment-summary-money">$4.77</div>
                     </div>
 
-                    <div clasName ="payment-summary-row total-row">
+                    <div className="payment-summary-row total-row">
                         <div>Order total:</div>
-                        <div clasName ="payment-summary-money">$52.51</div>
+                        <div className="payment-summary-money">$52.51</div>
                     </div>
 
-                    <button clasName ="place-order-button button-primary">
+                    <button className="place-order-button button-primary">
                         Place your order
                     </button>
                 </div>
